5649b18af46f0700f154a8cb0a98a2de58025705
Josueh123 <94991814+Josueh123@users.noreply.github.com>
2022-03-04 16:04:57 -0300

    Add files via upload

---
+++ Tower-Siege-1-Template-main/Tower-Siege-1-Template-main/SlingShot.js
@@ -0,0 +1,43 @@
+class SlingShot{
+
+    constructor(bodyA,pointB)
+	{
+		var options={
+            pointB: pointB,
+            
+            bodyA: bodyA, 
+
+            stiffness: 0.004,
+        
+            length: 1
+        }
+
+        this.sling = Constraint.create(options);
+        this.pointB = pointB
+ 		World.add(world, this.sling);
+
+	}
+
+    fly(){
+
+        this.sling.bodyA = null;
+
+    }
+
+	display()
+	{
+
+        if (this.sling.bodyA) {
+            
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            stroke("pink");
+
+            line(pointA.x,pointA.y,pointB.x,pointB.y)
+
+        }
+			
+	}
+
+}
